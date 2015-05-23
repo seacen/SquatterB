fc6b1af98141794905a1b5aaae24219963717bc8
Xichang Zhao <xichangz@student.unimelb.edu.au>
2015-05-23 15:20:27 +1000

    init

---
+++ Xichangz.java
@@ -0,0 +1,44 @@
+import java.io.PrintStream;
+
+import aiproj.squatter.Move;
+import aiproj.squatter.Piece;
+import aiproj.squatter.Player;
+
+
+
+/**
+ * Created by hexin on 5/05/15.
+ */
+public class Xichangz implements Player, Piece {
+
+	@Override
+	public int init(int n, int p) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Move makeMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int opponentMove(Move m) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWinner() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printBoard(PrintStream output) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
