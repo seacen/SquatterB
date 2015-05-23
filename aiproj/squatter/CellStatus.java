593b97c6079a0928268fd485111dc2b87110ea0f
Xichang Zhao <xichangz@student.unimelb.edu.au>
2015-05-24 03:26:55 +1000

    update Test, Please Try!, comments added, getWinner

--- aiproj/squatter/CellStatus.java
+++ aiproj/squatter/CellStatus.java
@@ -1,4 +1,9 @@
 package aiproj.squatter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  *   Piece:
  *      Define types of states that can appear on a board
@@ -11,4 +16,22 @@ public interface CellStatus extends Piece {
     public static final int CAPWHITE = 3,
                             CAPBLACK = 4,
                             CAPEMPTY = 5;
+    public static final Map<Integer, Character> mapToChar = 
+    		Collections.unmodifiableMap(
+    				new HashMap<Integer, Character>(6) {
+    					/**
+						 * 
+						 */
+						private static final long serialVersionUID = 1L;
+
+						{
+    						put(BLACK,'B');
+    						put(WHITE,'W');
+    						put(EMPTY,'+');
+    						put(CAPBLACK,'b');
+    						put(CAPWHITE,'w');
+    						put(CAPEMPTY,'-');
+    					}
+    				});
+    
 }
