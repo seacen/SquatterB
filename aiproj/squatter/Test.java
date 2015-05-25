fe64975249ef466b23a7753655b6544842e38ce4
Peter He <hexinpeter@gmail.com>
2015-05-25 17:36:34 +1000

    test case passes with loop_check

--- aiproj/squatter/Test.java
+++ aiproj/squatter/Test.java
@@ -18,30 +18,46 @@ public class Test implements CellStatus{
 		
 		player.init(n, p);
 		
-		/*
+
 		// TEST CONSTRUCTING BOARD FROM STDIN, TEST GETWINNER
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		player.init(n, p, br);
+
+        System.out.println("player:" + player.getRole());
+        System.out.println("free cell counts:" + player.getBoard().getFreeCellCount());
+		System.out.println("winner:" + player.getWinner());
+        System.out.println();
+
+        player.printBoard(System.out);
+
+        Move move = new Move();
+        move.P = p;
+        move.Col = 4;
+        move.Row = 1;
+
+        player.getBoard().checkLoop(move);
+
+        System.out.println();
+
+        player.printBoard(System.out);
+
+
+
+        // remind to put below codes to comment
+
+
 		
-		System.out.println(player.getBoard().getFreeCellCount());
-		
-		System.out.println(player.getWinner());
-		
-		// remind to put below codes to comment
-		*/
-		
-		
-		System.out.println(mapToChar.get(player.getRole()));
-		
-		player.printBoard(System.out);
-		System.out.println();
-		
-		player.makeMove();
-		prev=player.makeMove();
-		player.printBoard(System.out);
-		System.out.println();
+//		System.out.println(mapToChar.get(player.getRole()));
+//
+//		player.printBoard(System.out);
+//		System.out.println();
+//
+//		player.makeMove();
+//		prev=player.makeMove();
+//		player.printBoard(System.out);
+//		System.out.println();
 		
 		
 		/*
@@ -68,16 +84,16 @@ public class Test implements CellStatus{
 		*/
 		
 		
-		m.P=WHITE;
-		m.Row=n-1;
-		m.Col=n-1;
-		
-		if (player.opponentMove(m)==-1) {
-			System.out.println("ERROR!!\n");
-		}
-		
-		
-		player.printBoard(System.out);
+//		m.P=WHITE;
+//		m.Row=n-1;
+//		m.Col=n-1;
+//
+//		if (player.opponentMove(m)==-1) {
+//			System.out.println("ERROR!!\n");
+//		}
+//
+//
+//		player.printBoard(System.out);
 	}
 
 }
