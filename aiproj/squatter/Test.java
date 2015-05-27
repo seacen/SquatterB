65169b07cc668eeceac648acc436c216ba4bd534
Peter He <hexinpeter@gmail.com>
2015-05-27 16:03:45 +1000

    test code for BoardHash values

--- aiproj/squatter/Test.java
+++ aiproj/squatter/Test.java
@@ -9,7 +9,7 @@ public class Test implements CellStatus{
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		
-		int n=6;
+		int n=2;
 		int p=WHITE;
 		Move m = new Move();
 		Move prev;
@@ -26,22 +26,44 @@ public class Test implements CellStatus{
 
 		player.init(n, p, br);
 
-        System.out.println("player:" + player.getRole());
-		System.out.println("winner:" + player.getWinner());
-        System.out.println();
+        Board board1 = player.getBoard();
+        board1.printBoard(System.out);
+        int[] hash1 = board1.getHash();
+        for (int i : hash1) {
+            System.out.print(i + " ");
+        }
 
-        player.printBoard(System.out);
+//        System.out.println(board1.getHash()[0] + " " + board1.getHash()[1]);
 
-        Move move = new Move();
-        move.P = BLACK;
-        move.Col = 4;
-        move.Row = 1;
+//        br.close();
+
+//        BufferedReader newBR = new BufferedReader(new InputStreamReader(System.in));
+        Board board2 = new Board(board1.getDimension() ,br, player.getBoardHashArray());
+        board2.printBoard(System.out);
+//        System.out.println(board2.getHash()[0] + " " + board2.getHash()[1]);
+
+        int[] hash2 = board2.getHash();
+        for (int i : hash2) {
+            System.out.print(i + " ");
+        }
 
 
-        System.out.println();
-
-        player.opponentMove(move);
-        player.printBoard(System.out);
+//        System.out.println("player:" + player.getRole());
+//		System.out.println("winner:" + player.getWinner());
+//        System.out.println();
+//
+//        player.printBoard(System.out);
+//
+//        Move move = new Move();
+//        move.P = BLACK;
+//        move.Col = 4;
+//        move.Row = 1;
+//
+//
+//        System.out.println();
+//
+//        player.opponentMove(move);
+//        player.printBoard(System.out);
 
 
 
@@ -96,7 +118,7 @@ public class Test implements CellStatus{
 //
 //		player.printBoard(System.out);
 		
-		System.out.println(2^2);
+//		System.out.println(2^2);
 	}
 
 }
